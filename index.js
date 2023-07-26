1f38ef9a60dde91de874c582fb771813e71a0939
shrinidhihegde3 <shrinidhis007@gmail.com>
2023-07-26 22:18:45 +0530

    my first quote generator

---
+++ index.js
@@ -0,0 +1,28 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+const apiUrl =  "https://api.quotable.io/random";
+
+async function getquote(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+    console.log(data);
+}
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("quote");
+  
+    // Select the text field
+    //copyText.select();
+    //copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.innerText);
+  
+    // Alert the copied text
+    //alert("Copied the text: " + copyText.innerText);
+  }
+
+getquote(apiUrl);
